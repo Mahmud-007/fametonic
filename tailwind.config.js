a113ec0e9124450d1b72b7d2e984364f42c527db
Mahmud-007 <mahmud6799@gmail.com>
2025-04-19 18:08:31 +0600

    downgrade packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,css}"], // Adjust paths as needed
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
